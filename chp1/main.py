6e459a665d11bd6ca07974a910b243a8de903b8a
Sami <165500737+Sami21234@users.noreply.github.com>
2025-11-28 22:28:08 +0530

    Add files via upload

---
+++ chp1/main.py
@@ -0,0 +1,34 @@
+# First program
+print('Sami')
+print(12*8)
+
+# First task
+
+print("My Python journey like a Star \n"
+"I feel like a Predator \n"
+"I will deep down every par")
+
+# Comments
+# Comments are the line of codes or words that the programmer does not want to execute.
+# It is used to either explain a block of code or the particular topics
+#  Represented by --->  #
+
+# Indentation (aleast 1 space in the block )
+# Python uses indentation to indicate a block of code.
+
+if 5>2:
+    print("Five is Greater than two!\n")
+
+# Escape Sequence Characters
+# These are used to write the characters or sentences which are not directly can be written in the String in the (" ") 
+
+print("Hey this is the example of the Escape Sequence")
+print("Here for the next line we use this" "\n")
+print("Here for the Quote we use " "\"Backword Slash with Quotes\"")
+
+# Print functions
+
+print("Mohd Sami", 4,5,6,7,10, sep="-", end="\t LastDefaultWord")
+# Multiple and Mix texts and Numbers
+print(" I am", "Mohd Sami", "&", "I am", 20, "years old")
+
