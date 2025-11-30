a73d421013472d9fae98ff24099f52daf85bb2f7
Sami <165500737+Sami21234@users.noreply.github.com>
2025-11-30 14:25:54 +0530

    Operators and Type Casting

---
+++ chp3/Exercise-1.py
@@ -0,0 +1,12 @@
+# Create a Simple Calculator which is able to perform the operations like --> Addition, Subtraction, Multiplication, Division, and more
+
+a = 5
+b = 5
+
+print("The Value of", a, "+", b, "is :", a+b)
+print("The Value of", a, "-", b, "is :", a-b)
+print("The Value of", a, "*", b, "is :", a*b)
+print("The Value of", a, "/", b, "is :", a/b)
+print("The Value of", a, "//", b, "is :", a//b)
+print("The Value of", a, "%", b, "is :", a%b)
+print("The Value of", a, "**", b, "is :", a**b)
